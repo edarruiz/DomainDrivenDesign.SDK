286e446238a9ea2dff2961f5b702dc616df7de9b
Eric Roberto Darruiz <33941948+edarruiz@users.noreply.github.com>
2023-12-26 20:20:04 -0300

    Create IReason.cs

---
+++ src/DomainDrivenDesign.SDK/Errors/IReason.cs
@@ -0,0 +1,17 @@
+﻿namespace DomainDrivenDesign.SDK;
+
+/// <summary>
+/// Represents the interface implementation of the domain error reasons.
+/// </summary>
+public interface IReason {
+    /// <summary>
+    /// Gets the domain error message.
+    /// </summary>
+    string Message { get; }
+
+    /// <summary>
+    /// Gets or sets the <see cref="IDictionary{TKey, TValue}"/> containing the 
+    /// metadata information about the error details.
+    /// </summary>
+    IDictionary<string, object> Metadata { get; }
+}
