431839f799e06f044c09a41f4fb44380ef8042eb
Eric Roberto Darruiz <33941948+edarruiz@users.noreply.github.com>
2023-12-26 20:20:12 -0300

    Create IError.cs

---
+++ src/DomainDrivenDesign.SDK/Errors/IError.cs
@@ -0,0 +1,11 @@
+﻿namespace DomainDrivenDesign.SDK;
+
+/// <summary>
+/// Represents the interface implementation of domain errors.
+/// </summary>
+public interface IError : IReason {
+    /// <summary>
+    /// Gets the list of reasons for the error.
+    /// </summary>
+    IList<IError> Reasons { get; }
+}
