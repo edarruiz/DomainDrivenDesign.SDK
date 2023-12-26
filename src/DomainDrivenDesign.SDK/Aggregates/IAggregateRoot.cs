9604ba8f696ae728a852dcb4647b87c07af2ca84
Eric Roberto Darruiz <33941948+edarruiz@users.noreply.github.com>
2023-12-26 20:19:08 -0300

    Create IAggregateRoot.cs

---
+++ src/DomainDrivenDesign.SDK/Aggregates/IAggregateRoot.cs
@@ -0,0 +1,7 @@
+﻿namespace DomainDrivenDesign.SDK.Aggregates;
+
+/// <summary>
+/// Represents the implementation interface of the aggregate root for the domain aggregates.
+/// </summary>
+public interface IAggregateRoot {
+}
