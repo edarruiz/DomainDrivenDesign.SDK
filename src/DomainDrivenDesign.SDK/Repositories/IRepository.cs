94e715b4ccd5ac1e11ae0af8886b2eacc104749c
Eric Roberto Darruiz <33941948+edarruiz@users.noreply.github.com>
2023-12-26 21:48:13 -0300

    Update IRepository.cs

--- src/DomainDrivenDesign.SDK/Repositories/IRepository.cs
+++ src/DomainDrivenDesign.SDK/Repositories/IRepository.cs
@@ -12,4 +12,30 @@ internal interface IRepository<TAggregateRoot> where TAggregateRoot : IAggregate
     /// </summary>
     IUnitOfWork UnitOfWork { get; }
 
+    /// <summary>
+    /// Adds an entity to the domain aggregate.
+    /// </summary>
+    /// <typeparam name="TEntity">Type of the entity to be added to the aggregate.</typeparam>
+    /// <param name="entity">An entity to be added to the aggregate.</param>
+    /// <returns>Returns the entity newly added to the aggregate.</returns>
+    TEntity Add<TEntity>(TEntity entity) where TEntity : IEntity;
+
+    /// <summary>
+    /// Updates an entity's information in the domain aggregate.
+    /// </summary>
+    /// <typeparam name="TEntity">Type of the entity to be updated in the aggregate.</typeparam>
+    /// <param name="entity">An entity to be updated in the aggregate.</param>
+    /// <returns>Returns <see langword="true"/> if the entity was successfully updated
+    /// in the aggregate; <see langword="false"/> othwerwise.</returns>
+    bool Update<TEntity>(TEntity entity) where TEntity : IEntity;
+
+    /// <summary>
+    /// Remove an entity from the aggregate.
+    /// </summary>
+    /// <typeparam name="TEntity">Type of the entity to be removed from the aggregate.</typeparam>
+    /// <param name="entity">An entity to be removed from the aggregate.</param>
+    /// <returns>Returns <see langword="true"/> if the entity is successfully removed
+    /// from the aggregate; <see langword="false"/> otherwise.</returns>
+    bool Delete<TEntity>(TEntity entity) where TEntity : IEntity;
+
 }
